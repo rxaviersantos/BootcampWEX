985877fd99d9814645c16f875923e93ce2b822d3
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2023-10-03 19:43:43 -0300

    :wrench: add file tipo de dados

--- net/Program.cs
+++ net/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Clase 
+using net.Models;
+
+Pessoa P = new()
+{
+    Nome = "Rodrigo",
+    Idade = 36
+};
+P.Apresentar();
+
+
+//Variáveis 
+string apresentação = "Olá, seja bem vindo!";
+
+int quantidade = 1;
+double altura = 1.85;
+decimal preco = 1.50M;
+bool condicao = true;
+
+Console.WriteLine(apresentação);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura);
+Console.WriteLine("Valor da variável preco: " + preco);
+Console.WriteLine("Valor da variável condicao: " + condicao);
+
+//DateTime
+DateTime detaAtual = DateTime.Now.AddDays(3);
+Console.WriteLine(detaAtual.ToString("dd/MM/yyyy"));
