e8a57e93c129bb5cded6325524d0a0a9ba865293
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2023-10-19 23:56:42 -0300

    :hammer: Add or update development scripts

--- c#/propriedade/Program.cs
+++ c#/propriedade/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using namespace propriedade;
+
+Pessoa p1 = new Pessoa();
+p1.Nome = "Lorena";
+p1.Idade = 28;
+p1.Apresentar();
