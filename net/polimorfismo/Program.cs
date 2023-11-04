630b1617f060f5fe6838500677ee82c3c1b585e7
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2023-11-03 23:50:33 -0300

    :hammer: Add scripts de desenvolvimento

---
+++ net/polimorfismo/Program.cs
@@ -0,0 +1,14 @@
+﻿using csharp.Models;
+
+Aluno a1 = new Aluno();
+a1.Nome = "Lorena";
+a1.Idade = 20;
+a1.Email = "lorena@gmail.com";
+a1.Nota = 10;
+a1.Apresentar();
+
+Professor p1 = new Professor();
+p1.Nome = "Mayla";
+p1.Idade = 36;
+p1.Salario = 2000;
+p1.Apresentar();
