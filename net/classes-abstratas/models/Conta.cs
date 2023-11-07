c1c14a90f0bb56d0792518c44ee1596eac81343f
Rodrigo Xavier <rodrigomxsantos@gmail.com>
2023-11-06 22:52:04 -0300

    :tada: Begin a project

---
+++ net/classes-abstratas/models/Conta.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace classes_abstratas.models
+{
+    public class Conta
+    {
+        
+    }
+}
